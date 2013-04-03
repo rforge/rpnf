fd49cf2ddefb00c2d4ba028977de7028185b039b
starfox <starfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-03 20:43:13 +0000

    Website angepasst.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rpnf@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,9 +43,50 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
-
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<p> Download the most <b>recent</b> version of rpnf-package <a href="https://r-forge.r-project.org/R/?group_id=1632"><strong>here</strong></a>. </p>
+
+<h1>Examples</h1>
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tr>
+<td>
+<h2>Traditional sample plot </h2>
+<pre>
+Point & Figure Plot DAX30 (log) 
+--------+-----------------------
+ 7954.90|                      X
+ 7648.94|                      X
+ 7354.75|    X X               X
+ 7071.88|    XOXO            X X
+ 6799.88|    XOXO            XOX
+ 6538.35|    XOXO            XOX
+ 6286.87|X X XO O          X XOX
+ 6045.07|XOXOX  OX X       XOXOX
+ 5812.57|XOXOX  OXOXOX     XOXO 
+ 5589.01| O O   OXOXOXOX X XOX  
+ 5374.05|       O O OXOXOXOXOX  
+ 5167.35|           O OXOXOXO   
+ 4968.61|             OXO O     
+ 4777.51|             O         
+--------+-----------------------
+       Y|22222222222222222222222
+       Y|00000000000000000000000
+       Y|11111111111111111111111
+       Y|00000111111111111111122
+        |                       
+       M|00000000000000000111100
+       M|45678338888889999001147
+        |                       
+       D|00200130111130122000220
+       D|57152512015815527461933
+</pre>
+</td> 
+<td>
+<h2>Modern style plot</h2>
+<img src="sampleplot.png" border="0" alt="Modern style plot sample" />
+</td> 
+</tr>
+</table>
 
 </body>
 </html>
