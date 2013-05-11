a0bdb156d828a04d7beb24a417c1715ce0f591ed
starfox <starfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-11 22:07:04 +0000

    Corrected and improved calculation and plot of performance matrix.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rpnf@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/performanceMap.R
+++ pkg/R/performanceMap.R
@@ -1,31 +1,47 @@
 #' This function calculates an upper triangle performance map for a given time 
 #' series.
 #' 
-CalcPerformanceMap <- function(quotes, column = "close") {
-  start <- as.Date(index(quotes))
-  end <- as.Date(index(quotes))
-  s <- matrix(data=rep(x=as.numeric(data.xts[,column]),
-                       times=length(data.xts[,column])),
-              nrow=length(data.xts[,column]),
-              ncol=length(data.xts[,column]))
+CalcPerformanceMap <- function(quotes, column = "close", mode = c("abs","rel")) {
+  s <- matrix(data=rep(x=as.numeric(quotes[,column]),
+                       times=length(quotes[,column])),
+              nrow=length(quotes[,column]),
+              ncol=length(quotes[,column]))
   absdiff <- t(s)-s
-  #absdiff<-colnames(start)
-  #absdiff<-rownames(end)
-  # efficiently remove lower part of matrix
   absdiff[lower.tri(absdiff,diag=T)] <- 0
-  #View(absdiff)
-  absdiff
+  # modifiy result
+  if (mode == "abs")
+    result <- absdiff
+  else if (mode == "rel") 
+    result <- absdiff%*%diag(1/as.numeric(quotes[,column]))
+  else # default fallback to abs
+    result <- absdiff
+  # efficiently remove lower part of matrix
+  result[lower.tri(result,diag=T)] <- NA
+  rownames(result)<-as.character(index(quotes))
+  colnames(result)<-as.character(index(quotes))
+  # return result
+  result
 }
 
-# This function plots a performance map as an heatmap.
-PlotPerformanceMap <- function(quotes, column = "close") {
+#' This function plots a performance map as an heatmap.
+#'
+#' TODO: Create a proper colormap centered at 0 
+#' TODO: Label axes with date instead of numbers
+#' @seealso \url{http://www.nytimes.com/interactive/2011/01/02/business/20110102-metrics-graphic.html}
+PlotPerformanceMap <- function(quotes, column = "close", ...) {
   # determine performance map data
-  map <- CalcPerformanceMap(quotes,column)
+  map_abs <- CalcPerformanceMap(quotes,column,mode="abs")
+  map_rel <- CalcPerformanceMap(quotes,column,mode="rel")
   # prepare plot
   oldpar<-par()
   par(mfrow=c(2,1))
-  image(rownames(map),colnames(map),map)
-  plot(index(quotes),quotes$close)
-  lines(index(quotes),quotes$close)
+  image(index(quotes),index(quotes),t(map_abs),
+        main="Absolute return of investment",
+        xlab="End of investment",
+        ylab="Start of investment")
+  image(index(quotes),index(quotes),t(map_rel),
+        main="Relative return of investment",
+        xlab="End of investment",
+        ylab="Start of investment")
   par(oldpar)  
 }
